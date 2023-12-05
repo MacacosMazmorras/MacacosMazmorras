3d6d840c6d5001b477ccbc2d55016ae8e692796c
Marc Vallverdú <marc.vallverdu.98@gmail.com>
2023-12-05 17:48:52 +0100

    Added SignIn view, Updated UsuarioController

--- MacacosMazmorrasMVC/DAL/Conexion.cs
+++ MacacosMazmorrasMVC/DAL/Conexion.cs
@@ -2,7 +2,7 @@
 {
     public class Conexion
     {
-        public static string CadenaBBDD = "Data Source=79.143.90.12,54321;" +
+        public static string StringBBDD = "Data Source=79.143.90.12,54321;" +
                                           "Initial Catalog=MacacosYMazmorras;" +
                                           "User ID=sa;" +
                                           "Password=123456789;";
