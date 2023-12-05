f2941b1578cf0074c5d75a6e8afc9a69d6b28a63
sacom14 <samir.comas1@gmail.com>
2023-12-05 16:44:41 +0100

    samir edit spell class

--- MacacosMazmorrasMVC/Models/Spell.cs
+++ MacacosMazmorrasMVC/Models/Spell.cs
@@ -4,37 +4,35 @@
     {
         private int spellId;
         private string spellName;
-        private string spellDescription;
+        private string[] spellDescription;
         private string spellRange;
         private string spellDuration;
+        private string? spellMaterial;
         private bool spellConcentration;
         private int spellLevel;
-        private string? spellDamageType;
-        private string? spellDamage;
+
 
         public int SpellId { get { return spellId; } set {  spellId = value; } }
         public string SpellName { get {  return spellName; } set { spellName = value; } }
-        public string SpellDescription { get {  return spellDescription; } set { spellDescription = value; } }
+        public string[] SpellDescription { get {  return spellDescription; } set { spellDescription = value; } }
         public string SpellRange { get {  return spellRange; } set { spellRange = value; } }
         public string SpellDuration { get {  return spellDuration; } set { spellDuration = value; } }
+        public string? SpellMaterial { get { return spellMaterial; } set { spellMaterial = value; } }
         public bool SpellConcentration { get {  return spellConcentration; } set { spellConcentration = value; } }
         public int SpellLevel { get {  return spellLevel; } set { spellLevel = value; } }
-        public string? SpellDamageType { get {  return spellDamageType; } set { spellDamageType = value; } }
-        public string? SpellDamage { get {  return spellDamage; } set { spellDamage = value; } }
 
         public Spell() { }
 
-        public Spell(int spellId, string spellName, string spellDescription, string spellRange, string spellDuration, bool spellConcentration, int spellLevel, string? spellDamageType, string? spellDamage)
+        public Spell(int spellId, string spellName, string[] spellDescription, string spellRange, string spellDuration, string spellMaterial bool spellConcentration, int spellLevel)
         {
             SpellId = spellId;
             SpellName = spellName;
             SpellDescription = spellDescription;
             SpellRange = spellRange;
             SpellDuration = spellDuration;
+            SpellMaterial = spellMaterial;
             SpellConcentration = spellConcentration;
             SpellLevel = spellLevel;
-            SpellDamageType = spellDamageType;
-            SpellDamage = spellDamage;
         }
     }
 }
