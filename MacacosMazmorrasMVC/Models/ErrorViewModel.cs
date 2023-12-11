0a61c4b7484bb820e1587e141820a9e604bcf200
Marc Vallverdú <marc.vallverdu.98@gmail.com>
2023-12-11 19:39:18 +0100

    GitIgnore Try

--- MacacosMazmorrasMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MacacosMazmorrasMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
