abb0c406d39cbd60e8403bcb2b3f98837c752348
David Mendoza <ddmendozafernandez@gmail.com>
2023-12-11 15:41:55 +0100

    basura

--- MacacosMazmorrasMVC/obj/Release/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
+++ MacacosMazmorrasMVC/obj/Release/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab113c7db8d30e8445ad1090f3733f58e4679eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
