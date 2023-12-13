5f69eb4cb9c49c0341bc4936a964bce60043b8d4
David Mendoza <147835655+ddmendoza0@users.noreply.github.com>
2023-12-13 01:58:41 +0100

    Campaign
    Controller, view and error correction in DAL and Model

--- MacacosMazmorrasMVC/Controllers/CampaignController.cs
+++ MacacosMazmorrasMVC/Controllers/CampaignController.cs
@@ -2,6 +2,8 @@
 using MacacosMazmorrasMVC.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
 
 namespace MacacosMazmorrasMVC.Controllers
 {
@@ -14,9 +16,18 @@ namespace MacacosMazmorrasMVC.Controllers
         {
             campaignDAL = new CampaignDAL(Conexion.StringBBDD);
         }
+
         public IActionResult Index()
         {
-            return View();
+            int userId = HttpContext.Session.GetInt32("_UsuarioId") ?? 0;
+
+            // RECUPERAR EL USUARIOOOO!!
+            //
+            List<Campaign> lstCampaign = campaignDAL.ObtainAllUserCampaigns(100);
+            //
+            //
+
+            return View(lstCampaign);
         }
 
         public IActionResult NewCampaignForm()
@@ -35,22 +46,21 @@ namespace MacacosMazmorrasMVC.Controllers
             if (ModelState.IsValid)
             {
                 campaignDAL.InsertCampaign(newCampaign);
+                ViewBag.CreateCampaignSuccess = true;
                 return RedirectToAction("Index", "Campaign"); //redirect to Campaign
             }
 
             return View(newCampaign);
         }
 
-        public IActionResult UpdateCampaignForm()
+        public IActionResult UpdateCampaignForm(int campaignId)
         {
-            Campaign modifyCampaign = campaignDAL.ObtainUserCampaign(1);
+            // Retrieve the campaign using the campaign ID
+            Campaign modifyCampaign = campaignDAL.ObtainUserCampaign(campaignId);
 
-            ViewBag.CampaignName = modifyCampaign.CampaignName;
-            ViewBag.CampaignDesc = modifyCampaign.CampaignDesc;
-            ViewBag.CampaignMap = modifyCampaign.CampaignMap;
-
-            return View();
+            return View(modifyCampaign);
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -59,11 +69,22 @@ namespace MacacosMazmorrasMVC.Controllers
             if (ModelState.IsValid)
             {
                 campaignDAL.UpdateCampaign(updateCampaign);
+                ViewBag.UpdateCampaignSuccess = true;
                 return RedirectToAction("Index", "Campaign"); //redirect to Campaign
             }
 
             return View(updateCampaign);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCampaign(int campaignId)
+        {
+            // Delete the campaign from the database based on the ID
+            campaignDAL.DeleteCampaign(campaignId);
+
+            // Redirect to a success page or any other appropriate action
+            return RedirectToAction("Index", "Campaign");
+        }
     }
 }
