6896173a89361a87cb478b59400a37a1ae140b99
Marc Vallverdú <marc.vallverdu.98@gmail.com>
2023-12-15 19:42:52 +0100

    Commit para mi

--- MacacosMazmorrasMVC/obj/Debug/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
+++ MacacosMazmorrasMVC/obj/Debug/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc320150eedabbfa8b5161a990d01e9ec5aee570")]
 [assembly: System.Reflection.AssemblyProductAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
