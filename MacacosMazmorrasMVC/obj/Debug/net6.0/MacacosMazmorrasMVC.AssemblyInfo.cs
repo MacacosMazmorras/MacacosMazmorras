3752dc5ada4883d42ddf48973bd4c1c6964a7b68
Marc Vallverdú <marc.vallverdu.98@gmail.com>
2023-12-15 15:56:02 +0100

    Added class to character form

--- MacacosMazmorrasMVC/obj/Debug/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
+++ MacacosMazmorrasMVC/obj/Debug/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7dd0317ac529375c5631d72ed2da146df57619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
