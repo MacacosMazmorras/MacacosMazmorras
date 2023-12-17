2f7d765e274fcd7c50fdff6257009b335b914a84
Marc Vallverdú <marc.vallverdu.98@gmail.com>
2023-12-17 17:58:25 +0100

    NO PERDAMOS ESTO PORFAVOR

---
+++ MacacosMazmorrasMVC/ViewModels/UserHomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MacacosMazmorrasMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MacacosMazmorrasMVC.ViewModels
+{
+    public class UserHomeViewModel
+    {
+        public Usuario UserInfo { get; set; }
+        public List<Sesion> Sessions { get; set; }
+        public List<SheetCustom> SheetCustoms { get; set; }
+    }
+}
