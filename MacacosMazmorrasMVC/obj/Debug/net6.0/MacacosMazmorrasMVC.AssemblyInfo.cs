c4f3950aa7b571ea133b6e495f2426d004227fbf
Marc Vallverdú <marc.vallverdu.98@gmail.com>
2023-12-20 16:35:52 +0100

    Trash merge commit

--- MacacosMazmorrasMVC/obj/Debug/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
+++ MacacosMazmorrasMVC/obj/Debug/net6.0/MacacosMazmorrasMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46cbc5b94581b8a332764da9dd49e28deb76682")]
 [assembly: System.Reflection.AssemblyProductAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MacacosMazmorrasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
